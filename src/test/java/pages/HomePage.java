6baf2ce92512bbbf1dc2e386776803fa463e7131
VG <vusal_gasimov@yahoo.com>
2022-06-29 12:50:49 -0400

    new changes

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,7 +1,16 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class HomePage {
-// new old
-    //old new
-  
+
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//a[@class='appointment-btn scrollto']")
+    public WebElement makeAnAppointment;
 }
