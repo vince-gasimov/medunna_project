c3763ccd2a7a7811490c5e81921e295c5abe46eb
VG <vusal_gasimov@yahoo.com>
2022-07-02 11:04:53 -0400

    vusal changes

--- src/test/java/utilities/DBUtils.java
+++ src/test/java/utilities/DBUtils.java
@@ -21,9 +21,9 @@ public class DBUtils {
      * DBUtils.createConnection(); -> to connect to teh database
      */
     public static void createConnection() {
-        String url = "jdbc:postgresql://157.230.48.97:5432/gmibank_db";
-        String username="techprodb_user";
-        String password="Techpro_@126";
+        String url = "jdbc:postgresql://medunna.com:5432/medunna_db";
+        String username="medunnadb_user";
+        String password="Medunnadb_@129";
         try {
             connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
