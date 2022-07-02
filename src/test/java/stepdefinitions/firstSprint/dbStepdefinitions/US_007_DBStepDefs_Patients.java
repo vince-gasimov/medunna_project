2c8cf7cbc2ca284470721d8feef60e339363e573
VG <vusal_gasimov@yahoo.com>
2022-07-02 11:27:23 -0400

    new changes

--- src/test/java/stepdefinitions/firstSprint/dbStepdefinitions/US_007_DBStepDefs_Patients.java
+++ src/test/java/stepdefinitions/firstSprint/dbStepdefinitions/US_007_DBStepDefs_Patients.java
@@ -1,4 +1,4 @@
-package stepdefinitions.dbStepdefinitions;
+package stepdefinitions.firstSprint.dbStepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
--- src/test/java/stepdefinitions/firstSprint/uiStepdefinitions/US_007_AppointmentStepDefs.java
+++ src/test/java/stepdefinitions/firstSprint/uiStepdefinitions/US_007_AppointmentStepDefs.java
@@ -1,4 +1,4 @@
-package stepdefinitions.uiStepdefinitions;
+package stepdefinitions.firstSprint.uiStepdefinitions;
 
 import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
