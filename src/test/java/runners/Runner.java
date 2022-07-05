d3538b0c923d2067232723f94c87109da0ee4545
Kubra Ozyasar <kubraozyasar.35@gmail.com>
2022-07-05 14:58:48 -0600

    new commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,9 +12,9 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"
         },
         features = "./src/test/resources/features",
-        glue = {"stepdefinitions", "hooks"},
-        tags = "@US007_TC001",
-        dryRun = true
+        glue = {"stepdefinitions","hooks"},
+        tags = "@US004_SignIn",
+        dryRun = false
 )
 public class Runner {
 }
