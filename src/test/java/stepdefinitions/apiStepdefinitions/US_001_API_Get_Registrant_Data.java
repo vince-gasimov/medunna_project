cecf2aec4fbcd3d24ad6e8031cf1282627a4fb9e
Irfan <alpergazel@gmail.com>
2022-07-10 14:38:23 -0400

    irfan update

--- src/test/java/stepdefinitions/apiStepdefinitions/US_001_API_Get_Registrant_Data.java
+++ src/test/java/stepdefinitions/apiStepdefinitions/US_001_API_Get_Registrant_Data.java
@@ -1,6 +1,7 @@
 package stepdefinitions.apiStepdefinitions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
@@ -8,17 +9,18 @@ import io.restassured.response.Response;
 import pojos.US_01_Registrant;
 import utilities.ConfigurationReader;
 
-import java.util.Arrays;
-
+import static hooks.Hooks.spec;
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static utilities.Authentication.generateToken;
-import static utilities.US_001_TXTWriter.saveUiRegistrantData;
 
 public class US_001_API_Get_Registrant_Data {
 
     Response response;
-    US_01_Registrant[] US01Registrants;
+    US_01_Registrant[] registrants;
+
+    Faker faker = new Faker();
 
     @Given("user sends a get-request to get all registrants' data")
     public void user_sends_a_get_request_() {
@@ -29,7 +31,8 @@ public class US_001_API_Get_Registrant_Data {
                 "Content-Type", ContentType.JSON,
                 "Accept", ContentType.JSON
         ).when().get(ConfigurationReader.getProperty("registrant_endpoint"));
-        response.prettyPrint();
+
+             response.prettyPrint();
 
     }
 
@@ -37,33 +40,78 @@ public class US_001_API_Get_Registrant_Data {
     public void user_deserializes_the_response() throws Exception {
 
         ObjectMapper obj = new ObjectMapper();
-        US01Registrants = obj.readValue(response.asString(), US_01_Registrant[].class);
-        System.out.println(US01Registrants.length);
+        registrants = obj.readValue(response.asString(), US_01_Registrant[].class);
+
+//        System.out.println(registrants.length);
+//        System.out.println(registrants[6].getFirstName());
+//        System.out.println(registrants[6].getLastName());
+//        System.out.println(registrants[6].getEmail());
+//        System.out.println(registrants[6].getSsn());
 
 
+//        boolean flag = false;
+//        for (int i = 0; i < registrants.length; i++) {
+//
+//            if (registrants[i].getFirstName().contains("Team")) {
+//                System.out.println(registrants[i].getFirstName());
+//                System.out.println(registrants[i].getSsn());
+//                flag = true;
+//                break;
+//            }
+//        }
+//        assertTrue(flag);
 
-        //        for (Registrant registrant : registrants) {
+
+//        for (US_01_Registrant registrant : registrants) {
 //            List<String> list = Arrays.asList(registrant.getAuthorities());
 //            System.out.println(list);
 //        }
-
-        boolean flag = false;
-        for (int i = 0; i < US01Registrants.length; i++) {
-
-            if (US01Registrants[i].getLogin().contains("Team")) {
-                System.out.println(US01Registrants[i].getFirstName());
-                flag = true;
-                break;
-            }
-
-        }
-        assertTrue(flag);
     }
 
     @Then("user saves the data to the files and validates")
     public void user_saves_the_data_to_the_files_and_validates() {
-        saveUiRegistrantData(US01Registrants);
-        System.out.println(Arrays.stream(US01Registrants).allMatch( reg -> reg.getFirstName().contains("Team")));
+        saveUiRegistrantsData(registrants);
+
+
+    }
+
+    @Given("user sends a post-request to create a new registrant")
+    public void userSendsAPostRequestToCreateANewRegistrant() {
+
+        //1.setting the url
+        spec.pathParams("first", "api", "second", "register");
+
+        //2.set the request body
+        US_01_Registrant requestBody = new US_01_Registrant(
+                ("Team83_" + faker.name().firstName()),
+                ("PostRequest_" + faker.name().lastName()),
+                (faker.idNumber().ssnValid()),
+                ("Team83_" + faker.name().username()),
+                "en",
+                "123456789",
+                ("team83_" + faker.internet().emailAddress()),
+                true);
+
+        //3.setting the post request and get the response
+        response = given().spec(spec).contentType(ContentType.JSON).body(requestBody).when().post("/{first}/{second}");
+
+        //4.Do assertions
+        US_01_Registrant actual = response.as(US_01_Registrant.class);
+
+        assertEquals(requestBody.getSsn(), actual.getSsn());//asserting the ssn
+
+        assertEquals(requestBody.getFirstName(), actual.getFirstName());
+
+        assertTrue(actual.getFirstName().contains("Team83_"));
+
+        assertTrue(actual.getLogin().contains("Team83"));
+
+        System.out.println(actual.getFirstName());
+
+        System.out.println(actual.getSsn()); //print the ssn to the console to see if it is correct
+
+        saveApiNewRegistrantData(new US_01_Registrant[]{actual}); //save the new registrant to the file
+
 
 
     }
