afdfc3fd875f8763afdfdf9f8177689905b5866c
VG <vusal_gasimov@yahoo.com>
2022-07-12 19:45:07 -0400

    new updates

--- src/test/java/stepdefinitions/apiStepdefinitions/US_007_AppointmentApiStepDefs.java
+++ src/test/java/stepdefinitions/apiStepdefinitions/US_007_AppointmentApiStepDefs.java
@@ -3,28 +3,34 @@ package stepdefinitions.apiStepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import utilities.ConfigurationReader;
+
+import static utilities.ApiRequestUtils007.getRequest;
+import static utilities.Authentication.generateToken;
 
 public class US_007_AppointmentApiStepDefs {
 
-    @Given("generate the token US_{int}")
-    public void generate_the_token_us(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    String token = "";
+    String endPoint = ConfigurationReader.getProperty("us_007_appointments_url");
+    Response response;
+
+    @Given("generate the token US007")
+    public void generate_the_token_us() {
+        token = generateToken();
     }
-    @Given("user makes an appointment request with API US_{int}")
-    public void user_makes_an_appointment_request_with_api_us(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Given("user makes an appointment request with API US007")
+    public void user_makes_an_appointment_request_with_api_us() {
+        response = getRequest(token, endPoint);
+        response.prettyPrint();
     }
-    @When("user deserializes the appointments data US_{int}")
-    public void user_deserializes_the_appointments_data_us(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("user deserializes the appointments data US007")
+    public void user_deserializes_the_appointments_data_us() {
+
     }
-    @Then("user saves the data US_{int}")
-    public void user_saves_the_data_us(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("user saves the data US007")
+    public void user_saves_the_data_us() {
+
     }
 
 }
