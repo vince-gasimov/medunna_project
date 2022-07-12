42a9387394daf4085fabdf52302a68fe4133beac
VG <vusal_gasimov@yahoo.com>
2022-07-12 16:35:36 -0400

    commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US_005_db",
+        tags = "@US_007_GetRequest",
         dryRun =false
 )
 public class Runner {
