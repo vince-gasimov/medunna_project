331898386aa17cfbae3ae8960f57d51f6d5b8b8d
sedacoban <sedacoban00@gmail.com>
2022-07-12 14:49:15 -0400

    first commit

--- src/test/java/stepdefinitions/apiStepdefinitions/US_007_AppointmentsApiSteps.java
+++ src/test/java/stepdefinitions/apiStepdefinitions/US_007_AppointmentsApiSteps.java
@@ -10,21 +10,5 @@ import utilities.ConfigurationReader;
 import static io.restassured.RestAssured.given;
 
 public class US_007_AppointmentsApiSteps {
-    Response response;
-    US_007_Appointments appointments;
-    @Given("user sends a get request for user's data")
-    public void user_sends_a_get_request_for_user_s_data() {
-        response = given().
-                headers("Authorization", "Bearer " + ConfigurationReader.getProperty("us_007_api_token"), "Content-Type", ContentType.JSON, "Accept", ContentType.JSON).
-                when().get(ConfigurationReader.getProperty("us_007_get_appointments_api"));
-        response.prettyPrint();
-    }
-    @Given("user deserializes the user's data to java")
-    public void user_deserializes_the_user_s_data_to_java() {
-//        ObjectMa
-    }
-    @Then("user saves the users' data to correspondent files and validates")
-    public void user_saves_the_users_data_to_correspondent_files_and_validates() {
-
-    }
 }
+
