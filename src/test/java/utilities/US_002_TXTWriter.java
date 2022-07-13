47c2989f706ee543e40d060671f79ad0db217324
Sara Aksoy <eczglsm89@gmail.com>
2022-07-13 17:12:53 -0400

    gulsum version

---
+++ src/test/java/utilities/US_002_TXTWriter.java
@@ -0,0 +1,75 @@
+package utilities;
+
+import pojos.US_002_RegistrantPojo;
+
+import java.io.*;
+
+public class US_002_TXTWriter {
+    public static void saveUIRegistrantData(US_002_RegistrantPojo registrant){
+
+        try{
+            //File creation ==>> First parameter is the path of the file.that will be created.
+            FileWriter fw= new FileWriter(ConfigurationReader.getProperty("applicant_data"),false);
+            // true ==>> This file will not be deleted and re-updated. It will just overload in the same file again and again.
+            //If we have new record it will be overloaded.
+            // false ==>> It will delete the existing file and will recreate a new file, so it will update our file.
+
+            //Records creation
+            BufferedWriter bw= new BufferedWriter(fw); // It will create the records into fw object file location.
+            bw.append(registrant.toString()+"\n"); // "\n" will take all the records one by one into different lines.
+            bw.close();
+
+        }catch(Exception e){
+
+        }
+        // In our utilities class it is better to use try-catch blocks for handling exceptions.
+        // Try-catch blocks are more dynamic because whenever we call this method we don't need
+        // to handle it again and again.
+    }
+
+    public static void saveUIRegistrantData(US_002_RegistrantPojo[] registrants){
+
+        try {
+            FileWriter fw = new FileWriter(ConfigurationReader.getProperty("applicants_api_data"),false);
+
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for (int i=0; i<registrants.length;i++){
+                bw.append(registrants[i].toString()+"\n");
+            }
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    //From Kadir
+    public static String readFile(String filepath)
+    {
+        String returnFileContent="";
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            String st;
+            while ((st = br.readLine()) != null){
+                returnFileContent = returnFileContent +st +"\n";
+                System.out.println(st);
+            }
+        } catch(Exception e){
+
+        }
+
+        return returnFileContent;
+    }
+
+    public static void SavePojoToFile(String filePath, Object pojoObj){
+        try{
+            FileWriter fw2 = new FileWriter(filePath,false);
+            BufferedWriter bw = new BufferedWriter(fw2);
+            bw.append(pojoObj +"\n");
+            bw.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
