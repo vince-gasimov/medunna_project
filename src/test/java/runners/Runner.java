c67bfc67d1fbd994a646ed181addf9302feb03ee
Onder Onem <dronderonem@gmail.com>
2022-07-13 19:16:45 -0400

    last

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "  @US_006_Scenario_3",
-        dryRun =false
+        tags = "  @US_006",
+        dryRun = false
         )
 public class Runner {
 }
