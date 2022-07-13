82d1ed9d3a05647100556f30bcad3eda1c026e8b
Sadrettin Akkoyun <sadrettin.akkoyun10@gmail.com>
2022-07-12 23:50:44 -0400

    update

--- src/test/java/stepdefinitions/uiStepdefinitions/US_010_AppointmentsListStepDefs.java
+++ src/test/java/stepdefinitions/uiStepdefinitions/US_010_AppointmentsListStepDefs.java
@@ -14,18 +14,22 @@ public class US_010_AppointmentsListStepDefs {
 
     DoctorPage doctorPage = new DoctorPage();
     LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
     MyPagesAppointmentPage myPagesAppointmentPage = new MyPagesAppointmentPage();
     US_010_AppointmentsListPage appointmentsListPage = new US_010_AppointmentsListPage();
-    TimeSlots timeSlots=new TimeSlots();
+//    TimeSlots timeSlots=new TimeSlots();
     Actions actions = new Actions(Driver.getDriver());
-    public String patientID;
 
     @Given("doctor navigates to medunna.com")
     public void doctor_navigates_to_medunna_com() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("medunna_url"));
+        Driver.getDriver().get(ConfigurationReader.getProperty("base_url"));
     }
 
     @Given("doctor clicks on the user icon button")
+    public void doctor_clicks_on_the_user_icon_button(){
+        homePage.userIcon.click();
+    }
+
     @Then("doctor clicks on the Sign in button")
     public void doctor_clicks_on_the_Sign_in_button() throws InterruptedException{
         loginPage.signInLocation.click();
@@ -36,19 +40,17 @@ public class US_010_AppointmentsListStepDefs {
     public void doctor_writes_a_valid_username() {
         ReusableMethods.waitFor(2);
         ReusableMethods.waitForPageToLoad(7);
-        myPagesAppointmentPage.firstSignIn.click();
-        myPagesAppointmentPage.signIn.click();
-        loginPage.usernameBox.sendKeys(ConfigurationReader.getProperty("validDoctorUsername"));
+        loginPage.usernameBox.sendKeys("team83_filizdoctor2");
     }
 
     @Given("doctor writes a valid password")
     public void doctor_writes_a_valid_password() {
-        loginPage.passwordBox.sendKeys(ConfigurationReader.getProperty("validDoctorPassword"));
+        loginPage.passwordBox.sendKeys("filizdoctor2");
     }
 
     @And("doctor clicks on Sign in button")
     public void doctor_clicks_on_Sign_in_button() throws InterruptedException{
-        loginPage.signInLocation.click();
+        loginPage.signInButton.click();
         Thread.sleep(2);
     }
 
@@ -61,20 +63,15 @@ public class US_010_AppointmentsListStepDefs {
     @Given("doctor clicks on Appointments button")
     public void doctor_clicks_on_Appointments_button() throws InterruptedException{
         ReusableMethods.waitFor(2);
-        myPagesAppointmentPage.myPages.click();
+        myPagesAppointmentPage.myAppointments.click();
     }
 
-    @Given("doctor selects Appointment date from datefrom and dateto")
-    public void doctor_selects_appointment_date_from_datefrom_and_dateto() {
+    @Given("doctor selects Appointments startDate, endDate and status")
+    public void doctor_selects_appointments_date_startDate_and_endDate() {
+        Assert.assertTrue(Driver.waitForVisibility(myPagesAppointmentPage.id,1).isDisplayed());
         Assert.assertTrue(Driver.waitForVisibility(myPagesAppointmentPage.startDate,1).isDisplayed());
         Assert.assertTrue(Driver.waitForVisibility(myPagesAppointmentPage.endDate,1).isDisplayed());
+        Assert.assertTrue(Driver.waitForVisibility(myPagesAppointmentPage.status,1).isDisplayed());
+
     }
-
-    @Given("doctor should see Appointments list and time slots")
-    public void doctor_should_see_appointment_list_and_time_slots() {
-        Assert.assertTrue(doctorPage.timeSlot.isDisplayed());
-        Assert.assertTrue(doctorPage.appointmentList.isDisplayed());
-        }
-
-
 }
