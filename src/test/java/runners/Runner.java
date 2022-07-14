e3f497b3ade02cbadab550e095d05cc9bae395e5
Irfan <alpergazel@gmail.com>
2022-07-14 08:19:33 -0400

    irfan update

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US002_UI",
+        tags = "@PostApiRegistrants",
         dryRun =true
         )
 
--- src/test/java/stepdefinitions/apiStepdefinitions/US_015_API_Get_Patients_Data.java
+++ src/test/java/stepdefinitions/apiStepdefinitions/US_015_API_Get_Patients_Data.java
@@ -8,19 +8,24 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import pojos.US_015_Patient_Pojo;
 import pojos.US_015_UserPojo;
+import pojos.US_01_Registrant;
 import utilities.ConfigurationReader;
 
 import java.io.IOException;
+import java.util.List;
 
+import static hooks.Hooks.spec;
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 import static utilities.Authentication.generateToken;
-import static utilities.US_001_TXTWriter.saveApiAllPatientsData;
+import static utilities.US_001_TXTWriter.*;
 
 public class US_015_API_Get_Patients_Data {
 
     Response response;
     US_015_Patient_Pojo[] patients;
     US_015_UserPojo user;
+    List patientsList;
 
     Faker faker = new Faker();
 
@@ -44,7 +49,7 @@ public class US_015_API_Get_Patients_Data {
         //user deserializes the response into a nested POJO object*/
 
         ObjectMapper obj = new ObjectMapper();
-        patients = obj.readValue(response.asString(), US_015_Patient_Pojo[].class);
+        patientsList = obj.readValue(response.asString(), List.class);
 
         //user adds json to patients array of POJO objects
 
@@ -54,49 +59,10 @@ public class US_015_API_Get_Patients_Data {
 
     }
 
-        @Then("user saves the patients data to the file and validates")
-        public void user_saves_the_patients_data_to_the_file_and_validates () throws IOException {
-            saveApiAllPatientsData(patients);
-
-        }
-
-//    @Given("user sends a post-request to create a new registrant")
-//    public void userSendsAPostRequestToCreateANewRegistrant() {
-//
-//        //1.setting the url
-//        spec.pathParams("first", "api", "second", "register");
-//
-//        //2.set the request body
-//        US_01_Registrant requestBody = new US_01_Registrant(
-//                ("Team83_" + faker.name().firstName()),
-//                ("PostRequest_" + faker.name().lastName()),
-//                (faker.idNumber().ssnValid()),
-//                ("Team83_" + faker.name().username()),
-//                "en",
-//                "123456789",
-//                ("team83_" + faker.internet().emailAddress()),
-//                true);
-//
-//        //3.setting the post request and get the response
-//        response = given().spec(spec).contentType(ContentType.JSON).body(requestBody).when().post("/{first}/{second}");
-//
-//        //4.Do assertions
-//        US_01_Registrant actual = response.as(US_01_Registrant.class);
-//
-//        assertEquals(requestBody.getSsn(), actual.getSsn());//asserting the ssn
-//
-//        assertEquals(requestBody.getFirstName(), actual.getFirstName());
-//
-//        assertTrue(actual.getFirstName().contains("Team83_"));
-//
-//        assertTrue(actual.getLogin().contains("Team83"));
-//
-//        System.out.println(actual.getFirstName());
-//
-//        System.out.println(actual.getSsn()); //print the ssn to the console to see if it is correct
-//
-//        saveApiNewRegistrantData(new US_01_Registrant[]{actual}); //save the new registrant to the file
-
+    @Then("user saves the patients data to the file and validates")
+    public void user_saves_the_patients_data_to_the_file_and_validates() throws IOException {
+        saveApiAllPatientsData(patientsList);
 
     }
 
+ }
