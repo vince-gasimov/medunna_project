2d5931c22eed4f9027fa5537b1575d9686d62beb
Irfan <alpergazel@gmail.com>
2022-07-14 11:41:29 -0400

    irfan update

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "  @US_006",
+        tags = "@US_006",
         dryRun = false
 )
 
