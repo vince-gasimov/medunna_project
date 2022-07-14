efdb2b3cc0604a3d3303575d5eb47fdcdac5eda5
Irfan <alpergazel@gmail.com>
2022-07-14 11:40:48 -0400

    irfan update

--- src/test/java/stepdefinitions/apiStepdefinitions/US_015_API_Get_Patients_Data.java
+++ src/test/java/stepdefinitions/apiStepdefinitions/US_015_API_Get_Patients_Data.java
@@ -1,32 +1,28 @@
 package stepdefinitions.apiStepdefinitions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import pojos.US_015_Patient_Pojo;
 import pojos.US_015_UserPojo;
-import pojos.US_01_Registrant;
 import utilities.ConfigurationReader;
 
 import java.io.IOException;
 import java.util.List;
 
-import static hooks.Hooks.spec;
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static utilities.Authentication.generateToken;
-import static utilities.US_001_TXTWriter.*;
+import static utilities.US_001_TXTWriter.saveApiListPatientsData;
 
 public class US_015_API_Get_Patients_Data {
 
     Response response;
-    US_015_Patient_Pojo[] patients;
     US_015_UserPojo user;
-    List patientsList;
-
+    List<Object> patientList;
+    JsonPath jsonPath;
     Faker faker = new Faker();
 
     @Given("user sends a get-request to get all patients' data")
@@ -46,23 +42,23 @@ public class US_015_API_Get_Patients_Data {
     @Given("user deserializes the patients api response")
     public void user_deserializes_the_patients_api_response() throws IOException {
 
-        //user deserializes the response into a nested POJO object*/
-
-        ObjectMapper obj = new ObjectMapper();
-        patientsList = obj.readValue(response.asString(), List.class);
-
-        //user adds json to patients array of POJO objects
-
-
-        System.out.println(patients.length);
-
+        jsonPath = response.jsonPath();
+        patientList = jsonPath.get("");
+        System.out.println(patientList.size());
 
     }
+
 
     @Then("user saves the patients data to the file and validates")
     public void user_saves_the_patients_data_to_the_file_and_validates() throws IOException {
-        saveApiAllPatientsData(patientsList);
+        saveApiListPatientsData(patientList);
 
+        List<String> ssn = jsonPath.getList("user.ssn");
+        for(String s : ssn){
+            if(s.equals("654-54-5424")){
+                System.out.println("Found the patient");
+            }
+        }
     }
 
- }
+}
