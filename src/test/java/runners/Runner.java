3033a9c8c90e273e3e956493f19df7ca3d6cdf0f
Irfan <alpergazel@gmail.com>
2022-07-15 10:01:45 -0400

    irfan update4

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US_003_TC_007",
+        tags = "@database_medunna_patient_info",
         dryRun = false
 )
 
