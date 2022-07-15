f20a24ab77a0cbe09a9ff4c0925987d630a973af
Irfan <alpergazel@gmail.com>
2022-07-15 11:34:58 -0400

    irfan update4

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,9 +1,7 @@
 package runners;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
