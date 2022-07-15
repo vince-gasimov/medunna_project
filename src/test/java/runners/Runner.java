1f6f0c6f745a5dc54044bcd4a6a766ba5dc21167
VG <vusal_gasimov@yahoo.com>
2022-07-15 09:47:21 -0400

    vusal update

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US_003_TC_007",
+        tags = "@US007_TC001",
         dryRun =false
         )
 public class Runner {
