920b7240004d13fb29ffa0e27d877d2e4b0638ad
Irfan <alpergazel@gmail.com>
2022-07-16 09:12:20 -0400

    irfan update4

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,9 +12,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"
         },
         features = "./src/test/resources/features",
-
         glue = "stepdefinitions",
-
         tags = "@GetApiAppointmentStep",
         dryRun =false
 
