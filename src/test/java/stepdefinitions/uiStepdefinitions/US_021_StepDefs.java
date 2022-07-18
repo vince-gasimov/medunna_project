2100584838138cfe20bd23acec3331751fdd929f
Onder Onem <dronderonem@gmail.com>
2022-07-17 20:23:44 -0400

    2. sprint

--- src/test/java/stepdefinitions/uiStepdefinitions/US_021_StepDefs.java
+++ src/test/java/stepdefinitions/uiStepdefinitions/US_021_StepDefs.java
@@ -2,16 +2,24 @@ package stepdefinitions.uiStepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import pages.US_006_Pages;
 import pages.US_021_App_Page;
 import pages.US_021_Page;
 import utilities.ConfigurationReader;
+import utilities.Date;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class US_021_StepDefs {
 
     US_021_Page us_021_page = new US_021_Page();
-    US_021_App_Page us_021_app_page =new US_021_App_Page();
+    US_021_App_Page us_021_app_page = new US_021_App_Page();
+    Actions actions = new Actions(Driver.getDriver());
+
 
     @Given("Staff goes to Medunna URL")
     public void staff_goes_to_medunna_url() {
@@ -33,13 +41,13 @@ public class US_021_StepDefs {
 
     @Then("Staff enters username")
     public void staff_enters_username() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("us_021_username"));
+        us_021_page.usernameBox.sendKeys(ConfigurationReader.getProperty("us_021_username"));
     }
 
 
     @Then("Staff enters password")
     public void staff_enters_password() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("us_021_password"));
+        us_021_page.passwordBox.sendKeys(ConfigurationReader.getProperty("us_021_password"));
     }
 
 
@@ -51,7 +59,7 @@ public class US_021_StepDefs {
 
     @Then("Staff clicks on the MY PAGES button")
     public void staff_clicks_on_the_my_pages_button() {
-        us_021_page.myPagesButton.click();
+        ReusableMethods.waitForClickablility(us_021_page.myPagesButton, 3).click();
     }
 
 
@@ -69,30 +77,36 @@ public class US_021_StepDefs {
 
     @Then("Staff clicks Show Appointments button")
     public void staff_clicks_show_appointments_button() {
-        us_021_page.showAppButton.click();
+        ReusableMethods.waitForClickablility(us_021_page.showAppButton, 3).click();
     }
 
 
     @Then("Staff clicks Edit button")
     public void staff_clicks_edit_button() {
-        us_021_page.editButton.click();
+        ReusableMethods.waitForClickablility(us_021_page.editButton, 3).click();
     }
 
 
     @Then("Staff updates appointment")
     public void staff_updates_appointment() {
+        ReusableMethods.waitForClickablility(us_021_app_page.startDate, 3).sendKeys(Date.tomorrowsDate());
+        ReusableMethods.waitForClickablility(us_021_app_page.endDate, 3).sendKeys(Date.tomorrowsDate());
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
     }
 
 
     @Then("Staff clicks Save button")
     public void staff_clicks_save_button() {
-        us_021_app_page.save.click();
+        ReusableMethods.waitFor(2);
+        //ReusableMethods.waitForClickablility(us_021_app_page.save, 3).click();
+        Driver.clickWithJS(us_021_app_page.save);
     }
 
 
     @Then("Staff shows The appointment is updated with identifier popup")
     public void staff_shows_the_appointment_is_updated_with_identifier_popup() {
-        us_021_app_page.popup.isDisplayed();
+        Assert.assertTrue(ReusableMethods.waitForVisibility(us_021_app_page.popup, 3).isDisplayed());
+
     }
 
 
@@ -100,4 +114,36 @@ public class US_021_StepDefs {
     public void staff_close_the_browser() {
         Driver.closeDriver();
     }
+//********************************************************************************************************************
+
+//    @Then("staff clicks Status box")
+//    public void staff_clicks_status_box() {
+//      //  ReusableMethods.waitForClickablility(us_021_app_page.status, 3).click();
+//    }
+
+    @Then("staff may change {string}")
+    public void staffMayChange(String status) {ReusableMethods.waitForVisibility(us_021_app_page.status, 3);
+        Select select = new Select(us_021_app_page.status);
+        select.selectByVisibleText(status);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+    }
+
+
+    @Then("staff cannot change Status as COMPLETED")
+    public void staff_cannot_change_status_as_completed() {
+
+    }
+
+    @Then("staff should select the current doctor")
+    public void staff_should_select_the_current_doctor() {
+
+    }
+
+
+    @Then("Staff clicks Show Tests button")
+    public void staff_clicks_show_tests_button() {
+      us_021_app_page.ShowTests.click();
+    }
+
+
 }
