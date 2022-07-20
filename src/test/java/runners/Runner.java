89e770ef5acf322a6f979c9474e4accee1336e67
Sara Aksoy <eczglsm89@gmail.com>
2022-07-20 14:28:02 -0400

    new sprint codes UI

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US_021_Scenario_7",
+        tags = "@US028_UI",
         dryRun = false
 )
 public class Runner {
