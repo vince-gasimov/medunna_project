1558e7c1e978f42f07f5d23ea134fcc42df67fe6
Sara Aksoy <eczglsm89@gmail.com>
2022-07-25 18:26:27 -0400

    sprint2

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US22_EditTest ",
+        tags = "@US028",
         dryRun =false
 )
 public class Runner {
