048750deb439bd820371d8d6836260c4c6794e88
Sedat Arslanargin <chemistercan@gmail.com>
2022-07-28 23:24:27 -0400

    us25 commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US_08_user_password_segment",
+        tags = "   @US25_TC005",
         dryRun = false
 )
 public class Runner {
