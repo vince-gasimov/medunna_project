de61aadf53ce4bbcf36674da4abed779b46d8435
Irfan <alpergazel@gmail.com>
2022-07-29 10:58:35 -0400

    irfan update10

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@US_021",
+        tags = "@US028",
         dryRun =false
 )
 public class Runner {
